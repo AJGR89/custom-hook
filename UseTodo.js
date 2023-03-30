722b7a5fa69dba35428183946970ef2275a80e35
Alejandro J Gutierrez R <ajgr89@gmail.com>
2023-03-30 20:53:53 -0300

    primer commit

---
+++ UseTodo.js
@@ -0,0 +1,47 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../todoReducer";
+
+const initialState = [];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem("todos")) || [];
+};
+
+export const UseTodo = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = (newTodo) => {
+    const action = {
+      type: "[TODO] Add Todo",
+      payload: newTodo,
+    };
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = (id) => {
+    dispatch({
+      type: "[TODO] Remove Todo",
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = (id) => {
+    dispatch({
+      type: "[TODO] Toggle Todo",
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter((todo) => !todo.done).length,
+    handleDeleteTodo,
+    handleToggleTodo,
+    handleNewTodo,
+  };
+};
